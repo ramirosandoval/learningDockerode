3f54b8f9135901c7c837d9ad9ae41afcd01bc2cc
ramirosandoval <ramiro_sando@outlook.com>
2022-07-05 11:25:10 -0300

    front directory fixed

---
+++ front/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <web-terminal/>
+  </div>
+</template>
+
+<script>
+import WebTerminal from './components/WebTerminal.vue'
+
+export default {
+  name: 'App',
+  components: {WebTerminal},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+@import '../../node_modules/xterm/css/xterm.css';
+
+</style>
---
+++ front/src/components/WebTerminal.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <main>
+    <h1>Web terminal</h1>
+
+    <div ref="mainDiv"></div>
+  </main>
+</template>
+
+<script>
+import { Terminal } from 'xterm';
+import { AttachAddon } from 'xterm-addon-attach';
+
+  const ws = new WebSocket('ws://localhost:3000');
+  const term = new Terminal();
+  const attachAddon = new AttachAddon(ws);
+
+  term.loadAddon(attachAddon);
+
+  ws.onmessage = ({ data }) => {
+    if(data.stream){
+      data.stream.getReader();
+    }
+
+    term.onData((data) => {
+      console.log(data)
+      ws.send(data)
+    });
+  };
+
+  export default {
+    name: "DockerInfo",
+    mounted(){
+      term.open(this.$refs.mainDiv)
+      Object.freeze(term);  
+    }
+  };
+  
+</script>
+
+<style scoped>
+
+</style>
